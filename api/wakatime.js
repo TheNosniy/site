d3967cb05d67da9be8e15516606d46730ef993b2
TheNosniy <timaborodi2@gmail.com>
2026-03-20 23:35:39 +0500

    Update wakatime.js

--- api/wakatime.js
+++ api/wakatime.js
@@ -1,7 +1,7 @@
 // api/wakatime.js
 const https = require('https');
 
-const WAKA_KEY = 'waka_917a3b4a-789f-4bcf-bb15-d0a0af903fd4';
+const WAKA_KEY = 'waka_83f0384b-15f5-4fdd-8ae5-c8ddb4871936';
 
 module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
